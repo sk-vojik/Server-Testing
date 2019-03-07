2158ea537b879a18e181d111a86e402248893378
Scott Vojik <scott.vojik@gmail.com>
2019-03-07 14:21:51 -0800

    tests for people get working

--- api/server.spec.js
+++ api/server.spec.js
@@ -11,6 +11,28 @@ describe('server.js', () => {
       expect(res.status).toBe(200);
     });
 
+    it('should return json', async () => {
+      const res = await request(server).get('/');
+
+      expect(res.type).toBe('application/json')
+    });
+  });
+
+  describe('get /api/people ', () => {
+
+    it('should return OK 200', async () => {
+      const res = await request(server).get('/api/people');
+
+      expect(res.status).toBe(200);
+    });
+
+    it('should return an array of people objects in JSON', async () => {
+      const res = await request(server).get('/api/people');
+
+      expect(res.type).toBe('application/json')
+    });
+
+    
   });
   
 });
